55c1695118e782e1ae53e7646e6143e4738a7a78
lcc <leftcold@gmail.com>
2012-08-07 01:21:37 +0800

    添加测试数据

--- code/test/vim.py
+++ code/test/vim.py
@@ -1,6 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# w:fullFileName变量
+vimFullFileName = False
+# &enc变量
+vimEnc = 'utf-8'
+# w:acmd_auto_encode/g:acmd_auto_encode变量
+vimAcmdAutoEncode = '1'
+# w:commandCache变量
+vimCommandCache = ''
+
 def eval(param=''):
   if param == 'test eval':
     return 'success'
+  elif param == 'w:fullFileName':
+    return vimFullFileName
+  elif param == '&enc':
+    return vimEnc
+  elif param == 'exists("w:acmd_auto_encode") ? w:acmd_auto_encode : g:acmd_auto_encode':
+    return vimAcmdAutoEncode
   else:
     return 'failure'
 
