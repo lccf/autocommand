f2d335ca3ea99b4478b18d3da5fc12374ecc617d
lcc <leftcold@gmail.com>
2012-08-07 23:57:37 +0800

    添加 setCache、getCache单元测试

--- code/test/autocommand_test.py
+++ code/test/autocommand_test.py
@@ -82,15 +82,13 @@ def testGetConfig():
   rex = re.compile(r'^(?:\s+|)/\*.*?\*/(?:\s+|)$', re.M+re.S)
   jsons = rex.sub('', acmd.configContent)
   resultLv1 = [json.loads(jsons), dirLv1, '', '']
+  #resultLv1 = [json.loads(jsons), dirLv1, '', '.']
   #比较值输出结果
   if getResultLv1 == resultLv1:
     print '  test case 1:success'
   else:
-    print '  test case 1:failure'
-    print '  [resultLv1]:'
-    print '  '+str(resultLv1)
-    print '  [getResultLv1]'
-    print '  '+str(getResultLv1)
+    print '  test case 1:failure\n  [resultLv1]\n  %s\n  [getResultLv1]\n  %s' %(str(resultLv1), str(getResultLv1))
+    sys.exit()
 
   #计算二级目录
   if os.path.isdir('./testGetConfigLv2'):
@@ -107,15 +105,13 @@ def testGetConfig():
   #获取1级目录配置文件读取值
   getResultLv2 = acmd.getConfig(dirLv2)
   resultLv2 = [resultLv1[0], dirLv1, 'testGetConfigLv2/', '../']
+  #resultLv2 = [resultLv1[0], dirLv1, 'testGetConfigLv2/', '../.']
   #比较值输出结果
   if getResultLv2 == resultLv2:
     print '  test case 2:success'
   else:
-    print '  test case 2:failure'
-    print '  [resultLv2]:'
-    print '  '+str(resultLv2)
-    print '  [getResultLv2]'
-    print '  '+str(getResultLv2)
+    print '  test case 2:failure\n  [resultLv2]\n  %s\n  [getResultLv2]\n  %s' %(str(resultLv2), str(getResultLv2))
+    sys.exit()
 
   #计算三级目录
   if os.path.isdir('./testGetConfigLv3'):
@@ -132,15 +128,13 @@ def testGetConfig():
   #获取3级目录配置文件读取值
   getResultLv3 = acmd.getConfig(dirLv3)
   resultLv3 = [resultLv1[0], dirLv1, 'testGetConfigLv2/testGetConfigLv3/', '../../']
+  #resultLv3 = [resultLv1[0], dirLv1, 'testGetConfigLv2/testGetConfigLv3/', '../../.']
   #比较值输出结果
   if getResultLv3 == resultLv3:
     print '  test case 3:success'
   else:
-    print '  test case 3:failure'
-    print '  [resultLv3]:'
-    print '  '+str(resultLv3)
-    print '  [getResultLv3]'
-    print '  '+str(getResultLv3)
+    print '  test case 3:failure\n  [resultLv3]\n  %s\n  [getResultLv3]\n  %s' %(str(resultLv3), str(getResultLv3))
+    sys.exit()
 
   #清理复原
   os.chdir(currDir)
@@ -157,37 +151,36 @@ def testGetData():
   fileName2 = '说明.haml'
 
   # test case 1
+  # 针对情况：文件名及文件都为assic字符的情况下
   acmd.vim.vimFullFileName = os.path.realpath(pathName1 +'/'+ fileName1)
   getTc1Result = acmd.getData()
   tc1Result = [re.sub(r'\\', '/', os.path.realpath(pathName1 +'/'+ fileName1)), re.sub(r'\\', '/', os.path.realpath(pathName1))+'/', 'readme', 'haml']
+  #tc1Result = [re.sub(r'\\', '/', os.path.realpath(pathName1 +'/'+ fileName1)), re.sub(r'\\', '/', os.path.realpath(pathName1))+'/', 'readme', 'haml.']
 
   if getTc1Result == tc1Result:
     print '  test case 1:success'
   else:
-    print '  test case 1:failure'
-    print '  [getTc1Result]'
-    print '  '+str(getTc1Result)
-    print '  [tc1Result]'
-    print '  '+str(tc1Result)
+    print '  test case 1:failure\n  [tc1Result]\n  %s\n  [getTc1Result]\n  %s' %(str(tc1Result), str(getTc1Result))
+    sys.exit()
 
   # test case 2
+  # 针对情况：路径及文件名中带中文，且未开启文件名转码
   acmd.vim.vimFullFileName = os.path.realpath(pathName2 +'/'+ fileName2)
   getTc2Result = acmd.getData()
   codepage = locale.getdefaultlocale()[1].lower()
   tmp1 = re.sub( r'\\', '/', os.path.realpath(pathName2+'/'+fileName2))
   tmp2 = re.sub( r'\\', '/', os.path.realpath(pathName2).decode('utf-8').encode(codepage)+'/')
   tc2Result = [ tmp1, tmp2, re.sub( r'\.[^.]*$', '', fileName2).decode('utf-8').encode(codepage), 'haml' ]
+  #tc2Result = [ tmp1, tmp2, re.sub( r'\.[^.]*$', '', fileName2).decode('utf-8').encode(codepage), 'haml.' ]
 
   if getTc2Result == tc2Result:
     print '  test case 2:success'
   else:
-    print '  test case 2:failure'
-    print '  [getTc2Result]'
-    print '  '+str(getTc2Result)
-    print '  [tc2Result]'
-    print '  '+str(tc2Result)
+    print '  test case 2:failure\n  [tc2Result]\n  %s\n  [getTc2Result]\n  %s' %(str(tc2Result), str(getTc2Result))
+    sys.exit()
 
   # test case 3
+  # 针对情况：路径及文件名中带中文，且开启文件名转码
   acmd.vim.vimFullFileName = os.path.realpath(pathName2 +'/'+ fileName2)
   acmd.vim.vimAcmdAutoEncode = '0'
   getTc3Result = acmd.getData()
@@ -195,15 +188,83 @@ def testGetData():
   tmp1 = re.sub( r'\\', '/', os.path.realpath(pathName2)+'/'+fileName2)
   tmp2 = re.sub( r'\\', '/', os.path.realpath(pathName2).decode('utf-8').encode(codepage)+'/')
   tc3Result = [ tmp1, tmp2, re.sub( r'\.[^.]*$', '', fileName2), 'haml' ]
+  #tc3Result = [ tmp1, tmp2, re.sub( r'\.[^.]*$', '', fileName2), 'haml.' ]
 
   if getTc3Result == tc3Result:
     print '  test case 3:success'
   else:
-    print '  test case 3:failure'
-    print '  [getTc3Result]'
-    print '  '+str(getTc3Result)
-    print '  [tc3Result]'
-    print '  '+str(tc3Result)
+    print '  test case 3:failure\n  [tc3Result]\n  %s\n  [getTc3Result]\n  %s' %(str(tc3Result), str(getTc3Result))
+    sys.exit()
+# }}}
+
+# testSetCache 测试设置缓存 {{{
+def testSetCache():
+  print '\n[test] setCache'
+
+  testPath = os.path.realpath('./testSetCache')
+  # test case 1
+  testCommand = ['haml -nq test.haml test.html', 'mv test.html ../']
+  acmd.setCache(testPath, testCommand)
+  tc1Result = '@'+re.sub(r'\\', '/', testPath)+'|'+'|'.join(testCommand)
+  #tc1Result = '.@'+re.sub(r'\\', '/', testPath)+'|'+'|'.join(testCommand)
+  getTc1Result = acmd.vim.vimCommandCache
+  if getTc1Result == tc1Result:
+    print '  test case 1:success'
+  else:
+    print '  test case 1:failure\n  [tc1Result]\n  %s\n  [getTc1Result]\n  %s' %(str(tc1Result), str(getTc1Result))
+    sys.exit()
+
+  acmd.vim.vimCommandCache=''
+
+  # test case 2
+  testCommand = ['sass te|st.sass te|st.css', 'mv te|st.css ../css']
+  acmd.setCache(testPath, testCommand)
+  tc2Result = '@'+re.sub(r'\\', '/', testPath)+'|'+re.sub( r'@', '|', re.sub( r'\|', '\|', '@'.join(testCommand)))
+  #tc2Result = '.@'+re.sub(r'\\', '/', testPath)+'|'+re.sub( r'@', '|', re.sub( r'\|', '\|', '@'.join(testCommand)))
+  getTc2Result = acmd.vim.vimCommandCache
+  if getTc2Result == tc2Result:
+    print '  test case 2:success'
+  else:
+    print '  test case 2:failure\n  [tc2Result]\n  %s\n  [getTc2Result]\n  %s' %(str(tc2Result), str(getTc2Result))
+    sys.exit()
+
+  acmd.vim.vimCommandCache=''
+
+# }}}
+
+# testGetCache 测试获取缓存 {{{
+def testGetCache():
+  print '\n[test] getCache'
+
+  testPath = os.path.realpath('./testGetCache')
+  # test case 1
+  testCommand = ['haml -nq test.haml test.html', 'mv test.html ../']
+  acmd.setCache(testPath, testCommand)
+  tc1Result = [re.sub(r'\\', '/', testPath), testCommand]
+  #tc1Result = [re.sub(r'\\', '/.', testPath), testCommand]
+  getTc1Result = acmd.getCache()
+  if getTc1Result == tc1Result:
+    print '  test case 1:success'
+  else:
+    print '  test case 1:failure\n  [tc1Result]\n  %s\n  [getTc1Result]\n  %s' %(str(tc1Result), str(getTc1Result))
+    sys.exit()
+
+  acmd.vim.vimCommandCache=''
+
+  # test case 2
+  testCommand = ['sass te|st.sass te|st.css', 'mv te|st.css ../css']
+  acmd.setCache(testPath, testCommand)
+  tc2Result = '@'+re.sub(r'\\', '/', testPath)+'|'+re.sub( r'@', '|', re.sub( r'\|', '\|', '@'.join(testCommand)))
+  #tc2Result = '.@'+re.sub(r'\\', '/', testPath)+'|'+re.sub( r'@', '|', re.sub( r'\|', '\|', '@'.join(testCommand)))
+  getTc2Result = acmd.vim.vimCommandCache
+  if getTc2Result == tc2Result:
+    print '  test case 2:success'
+  else:
+    print '  test case 2:failure\n  [tc2Result]\n  %s\n  [getTc2Result]\n  %s' %(str(tc2Result), str(getTc2Result))
+    sys.exit()
+
+  acmd.vim.vimCommandCache=''
+
 # }}}
 
 # loadAcmd 加载autocommand模块 {{{
@@ -232,6 +293,8 @@ def testTest():
   testVimInterface()
   testGetConfig()
   testGetData()
+  testSetCache()
+  testGetCache()
 
 if __name__ == '__main__':
 
