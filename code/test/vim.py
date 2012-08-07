00e02e4ee94bb39578760ea236e28625515c71da
lcc <leftcold@gmail.com>
2012-08-07 23:37:53 +0800

    添加vimCommandCache变量，为setCache、getCache单元测试做铺垫

--- code/test/vim.py
+++ code/test/vim.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import re
 # w:fullFileName变量
 vimFullFileName = False
 # &enc变量
@@ -18,12 +19,19 @@ def eval(param=''):
     return vimEnc
   elif param == 'exists("w:acmd_auto_encode") ? w:acmd_auto_encode : g:acmd_auto_encode':
     return vimAcmdAutoEncode
+  elif param == 'w:commandCache':
+    return vimCommandCache
   else:
     return 'failure'
 
 def command(param=''):
   if param == 'test command':
     return 'success'
+  elif param.index('let w:commandCache') > -1:
+    global vimCommandCache
+    commandCache = re.match(r'let w:commandCache="(?P<commandCache>[^"]+)"', param)
+    if commandCache and commandCache.group('commandCache'):
+      vimCommandCache = commandCache.group('commandCache')
   else:
     return 'failure'
 # vim:sw=2:ts=2:sts=2:et:fdm=marker:fdc=1
